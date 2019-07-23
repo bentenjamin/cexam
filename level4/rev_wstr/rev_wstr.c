e5f9854670b898a5f317547804d1526b180f5304
Benjamin WEBB <bwebb@c3r4s4.wethinkcode.co.za>
2019-07-23 13:01:08 +0200

    rev_wstr

---
+++ level4/rev_wstr/rev_wstr.c
@@ -0,0 +1,33 @@
+#include <unistd.h>
+
+int		main(int argc, char **argv)
+{
+	int	i;
+	int	len;
+	int	k;
+
+	if ((argc != 2) || (argv[1] == NULL) || (argv[1][0] == '\0'))
+	{
+		write(1, "\n", 1);
+		return (1);
+	}
+	len = 0;
+	while (argv[1][len])
+		len++;
+	while (len > -1)
+	{
+		i = 0;
+		k = 0;
+		if ((argv[1][len] == '\t') || (len == 0) || (argv[1][len] == ' '))
+		{
+			i = (len == 0) ? len : len + 1;
+			while (!((argv[1][i] == '\t') || (argv[1][i] == ' ')) && argv[1][i])
+				write(1, &argv[1][i++], 1);
+			if (len != 0)
+				write(1, " ", 1);
+		}
+		len--;
+	}
+	write(1, "\n", 1);
+	return (1);
+}
