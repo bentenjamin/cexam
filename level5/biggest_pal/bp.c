cd4f9fc9b6aa931b356b08adb861d0e45031c6bc
Benjamin WEBB <bwebb@c3r4s4.wethinkcode.co.za>
2019-07-31 16:13:48 +0200

    tryna fiqure out bigpal

--- level5/biggest_pal/bp.c
+++ level5/biggest_pal/bp.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   biggesst_pal.c                                     :+:      :+:    :+:   */
+/*   bp.c                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: bwebb <bwebb@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/31 14:08:53 by bwebb             #+#    #+#             */
-/*   Updated: 2019/07/31 14:31:16 by bwebb            ###   ########.fr       */
+/*   Updated: 2019/07/31 16:13:02 by bwebb            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,30 +23,32 @@ int ispal(char *s, int a, int z)
 int main(int ac, char **av)
 {
     int i;
+    int j;
     int k;
-    int len;
-    char *p;
     
-    if (ac != 2)
+    if (ac != 2 || av[1][0] == '\0')
     {
         write(1, "\n", 1);
         return (0);
     }
+    if (av[1][1] == '\0')
+        write(1, av[1], 1);
     i = 0;
-    len = 0;
-    while (av[1][i + 1])
+    j = 0;
+    while (av[1][j + 1])
+        j++;
+    while (i < j)
     {
-        k = i;
-        while (av[1][++k])
-            if (ispal(av[1], i, k) && (k - i >= len))
-                {
-                    len = k - i + 1;
-                    p = av[1] + i;
-                }
+        k = -1;
+        while (++k <= i)
+            if (ispal(av[1], i - k, j - k))
+            {
+                write(1, av[i] + i - k, j - k + 1);
+                write(1, "\n", 1);
+                return (0);   
+            }
         i++;
     }
-    if (len)
-        write(1, p, len);
     write(1, "\n", 1);
     return (0);
 }
