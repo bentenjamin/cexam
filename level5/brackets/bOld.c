8e31a2346854fbc99033c2d1cfc8a596372f9815
Benjamin WEBB <bwebb@c3r6s9.wethinkcode.co.za>
2019-08-13 12:48:22 +0200

    improved brackets

---
+++ level5/brackets/bOld.c
@@ -0,0 +1,43 @@
+#include <unistd.h>
+
+int mtch(char c, char x)
+{
+    if (c == '(' && x == ')')
+        return (1);
+    if (c == '{' && x == '}')
+        return (1);
+    if (c == '[' && x == ']')
+        return (1);
+    return (0);//im not gonna explain what this does
+}
+
+int brack(char *s)
+{
+    int stk[100];//our stack for pushing and popping
+    int i;//counter
+
+    i = -1;//-1 since s[++i]
+    stk[0] = 0;//init stk[0] as the counter as the stack. therefore stk[stk[0]] is stack top
+    while (s[++i])//run till end of string (duh)
+        if (s[i] == '(' || s[i] == '[' || s[i] == '{')
+            stk[++stk[0]] = s[i];//if bracket is valid increase stk and slap it on there
+        else if (mtch(stk[stk[0]], s[i]))
+            stk[0]--;//if char is valid closing bracket and it matches our stk top, push off stack
+        else if ((s[i] == ')' || s[i] == ']' || s[i] == '}') && !stk[0])//not really happy how i did this cause i test if it is a valid closing bracket twice but it looks good and im too lazy to find a good alternative
+            return (0);//if we have a closing bracket to an empty stk or wrong top its invalid
+    if (stk[0])//if theres something in the stack then its invalid
+        return (0);
+    return (1);//stack is empty
+}
+
+int main(int ac, char **av)
+{
+    int i;
+
+    i = 1;
+    while (i < ac)
+        (brack(av[i++])) ? write(1, "OK\n", 3) : write(1, "Error\n", 6);//if our bracket string is true print ye else print nah
+    if (ac == 1)
+        write (1, "\n", 1);
+    return (0);
+}
--- level5/brackets/brackets.c
+++ level5/brackets/brackets.c
@@ -1,33 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   brack.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bwebb <bwebb@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/08/13 11:44:44 by bwebb             #+#    #+#             */
+/*   Updated: 2019/08/13 12:47:23 by bwebb            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 
-int mtch(char c, char x)
+int brack(char *s, char c)
 {
-    if (c == '(' && x == ')')
+    while (s && !((s[0] == '{') || (s[0] == '[') || (s[0] == '(') || (s[0] == '}') || (s[0] == ']') || (s[0] == ')')))
+        s++;
+    if ((s[0] == '{') || (s[0] == '[') || (s[0] == '('))
+        return (brack(s + 1, s[0]));
+    if (((c == '{') && (s[0] == '}')) || ((c == '[') && (s[0] == ']')) || ((c == '(') && (s[0] == ')')))
         return (1);
-    if (c == '{' && x == '}')
-        return (1);
-    if (c == '[' && x == ']')
-        return (1);
-    return (0);//im not gonna explain what this does
-}
-
-int brack(char *s)
-{
-    int stk[100];//our stack for pushing and popping
-    int i;//counter
-
-    i = -1;//-1 since s[++i]
-    stk[0] = 0;//init stk[0] as the counter as the stack. therefore stk[stk[0]] is stack top
-    while (s[++i])//run till end of string (duh)
-        if (s[i] == '(' || s[i] == '[' || s[i] == '{')
-            stk[++stk[0]] = s[i];//if bracket is valid increase stk and slap it on there
-        else if (mtch(stk[stk[0]], s[i]))
-            stk[0]--;//if char is valid closing bracket and it matches our stk top, push off stack
-        else if ((s[i] == ')' || s[i] == ']' || s[i] == '}') && !stk[0])//not really happy how i did this cause i test if it is a valid closing bracket twice but it looks good and im too lazy to find a good alternative
-            return (0);//if we have a closing bracket to an empty stk or wrong top its invalid
-    if (stk[0])//if theres something in the stack then its invalid
-        return (0);
-    return (1);//stack is empty
+    return (0);
 }
 
 int main(int ac, char **av)
@@ -36,7 +29,7 @@ int main(int ac, char **av)
 
     i = 1;
     while (i < ac)
-        (brack(av[i++])) ? write(1, "OK\n", 3) : write(1, "Error\n", 6);//if our bracket string is true print ye else print nah
+        (brack(av[i++], 'x')) ? write(1, "OK\n", 3) : write(1, "Error\n", 6);//if our bracket string is true print ye else print nah
     if (ac == 1)
         write (1, "\n", 1);
     return (0);
