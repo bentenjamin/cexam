6102cae6a968cd99108ed6c6d5ddf34275c0bd22
Benjamin WEBB <bwebb@c3r5s9.wethinkcode.co.za>
2019-08-16 16:30:00 +0200

    f

--- level5/brackets/brackets.c
+++ level5/brackets/brackets.c
@@ -10,6 +10,8 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+
 int brack(char *s)
 {
     int stk[100];                                                                                                                   //our stack for pushing and popping
